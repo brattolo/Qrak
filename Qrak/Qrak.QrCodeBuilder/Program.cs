ae98034cd4db6f2ae688426fb2b62b8e7f26d574
Bart Gevaert <bart.gevaert@outlook.com>
2018-09-13 22:20:00 +0200

    Project folders Console app to start from

---
+++ Qrak/Qrak.QrCodeBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Qrak.QrCodeBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
