7696685c82ce24885ef10fcbfdbce008d252f070
Bart Gevaert <bart.gevaert@outlook.com>
2018-09-13 22:28:58 +0200

    Added drawing library Added first image generation

--- Qrak/Qrak.QrCodeBuilder/Program.cs
+++ Qrak/Qrak.QrCodeBuilder/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
 namespace Qrak.QrCodeBuilder
 {
@@ -6,7 +9,11 @@ namespace Qrak.QrCodeBuilder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DirectoryInfo directoryInfo = Directory.CreateDirectory("output");
+            using (Image<Rgba32> image = new Image<Rgba32>(100, 100))
+            {
+                image.Save("output/qrCode.png");
+            }
         }
     }
 }
